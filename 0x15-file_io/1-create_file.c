5bed7ec8a3ec4185f13d10201e6ee715889f175d
Mwakai <mwakaimwambala@gmail.com>
2023-09-07 09:58:53 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file -creates an array of chars, and initializes
  *
  * @text_content: is a NULL terminated string to write to the file
- * @Filename: is the name of the file to create
+ * @filename: is the name of the file to create
  *
  * Return: 1 on success, -1 on failure
  */
